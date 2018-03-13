7c5ea1b4265be00db629a7e2b25a812d01fcf979
Dmitry Filonenko <d.filonenko@kezzler.com>
2018-03-13 16:27:40 +0100

    Show customer ID and button for editing

--- src/main/web/src/app/customers/Customer.ts
+++ src/main/web/src/app/customers/Customer.ts
@@ -1,13 +1,15 @@
 import { Address } from "./Address";
 
 export class Customer { 
+  customerId: string;
   firstName: string;
   lastName: string;
   companyName: string;
   address: Address;
   email: string;
  
-  constructor(firstName: string, lastName: string, companyName: string, address: Address, email: string) {
+  constructor(customerId: string, firstName: string, lastName: string, companyName: string, address: Address, email: string) {
+    this.customerId = customerId;
     this.firstName = firstName;
     this.lastName = lastName;
     this.companyName = companyName;
