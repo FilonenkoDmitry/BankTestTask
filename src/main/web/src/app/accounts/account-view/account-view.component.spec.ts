951ab7db243e73cb51e48254fb58c02da17d0c28
Dmitry Filonenko <d.filonenko@kezzler.com>
2018-03-19 14:03:28 +0100

    Fix test compilation

--- src/main/web/src/app/accounts/account-view/account-view.component.spec.ts
+++ src/main/web/src/app/accounts/account-view/account-view.component.spec.ts
@@ -1,15 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountViewComponent } from './account-view.component';
+import { AccountsService } from '../accounts.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AccountViewComponent', () => {
+  let accountsService: AccountsService;
   let component: AccountViewComponent;
   let fixture: ComponentFixture<AccountViewComponent>;
 
+  const stubAccountsService = jasmine.createSpyObj('AccountsService', ['findDetailsById', 'findBalanceById']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AccountViewComponent ]
+      imports: [ RouterTestingModule.withRoutes([]) ],
+      declarations: [ AccountViewComponent ],
+      providers: [{provide: AccountsService, useValue: stubAccountsService}]
     })
+    .overrideComponent(AccountViewComponent, {set: { providers: [
+      { provide: AccountsService, useValue: stubAccountsService}
+    ]}})
     .compileComponents();
   }));
 
